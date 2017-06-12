cd636d7a1870720b99017c8bfca22a0afddd1e59
Chong-Lim Kim <clkim@ieee.org>
2017-06-11 21:57:55 -0400

    update unit/instrumented tests to use ActivityTestRule instead of deprecated ActivityInstrumentationTestCase2

--- app/src/androidTest/java/net/gouline/dagger2demo/activity/AlbumSearchActivityTest.java
+++ app/src/androidTest/java/net/gouline/dagger2demo/activity/AlbumSearchActivityTest.java
@@ -1,18 +1,22 @@
 package net.gouline.dagger2demo.activity;
 
-import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.RecyclerView;
-import android.test.ActivityInstrumentationTestCase2;
-import android.widget.TextView;
 
 import net.gouline.dagger2demo.R;
 import net.gouline.dagger2demo.rest.ITunesService;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Mostly following:
@@ -20,57 +24,32 @@ import org.junit.runner.RunWith;
  *  https://developer.android.com/topic/libraries/testing-support-library/index.html#AndroidJUnitRunner
  *  https://developer.android.com/training/testing/unit-testing/instrumented-unit-tests.html#build
  *
- * Created by clkim on 10/19/15, updated 06/11/16
+ * Created by clkim on 10/19/15, updated 06/11/16, updated 06/11/17
  */
 @RunWith(AndroidJUnit4.class)
-public class AlbumSearchActivityTest extends ActivityInstrumentationTestCase2<AlbumSearchActivity> {
-    private AlbumSearchActivity albumSearchActivity;
-    private RecyclerView recyclerView;
-    private TextView emptyView;
-    private ITunesService mITunesService;
+@SmallTest
+public class AlbumSearchActivityTest {
 
-    public AlbumSearchActivityTest() {
-        super(AlbumSearchActivity.class);
-    }
+    @Rule
+    public ActivityTestRule<AlbumSearchActivity> asaActivityTestRule = new ActivityTestRule<>(AlbumSearchActivity.class);
 
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        // Injecting the Instrumentation instance is required
-        // for your test to run with AndroidJUnitRunner.
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-
-        albumSearchActivity = getActivity();
-        recyclerView = (RecyclerView) albumSearchActivity.findViewById(R.id.recycler_view);
-        emptyView = (TextView) albumSearchActivity.findViewById(R.id.empty_view);
-
-        // accessing a Kotlin property in Activity class
-        //  exposed because it is a late-initialized property
-        mITunesService = albumSearchActivity.getMITunesService();
-    }
 
     @Test
     public void testPreConditions() {
-        assertNotNull(albumSearchActivity);
-        assertNotNull(recyclerView);
-        assertNotNull(emptyView);
+        onView(withId(R.id.recycler_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.empty_view)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testITuneServiceIsInjected() {
+        ITunesService mITunesService = asaActivityTestRule.getActivity().getMITunesService();
         // test for successful Dagger 2 injection in the Activity class
         assertNotNull(mITunesService);
     }
 
     @Test
     public void testEmptyTextView_labelText() {
-        String expected = albumSearchActivity.getString(R.string.search_empty);
-        String actual = emptyView.getText().toString();
-        assertEquals(expected, actual);
+        onView(withId(R.id.empty_view)).check(matches(withText(R.string.search_empty)));
     }
 
-    @After
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
 }
